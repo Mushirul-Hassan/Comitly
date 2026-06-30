bfe759c635ea13aabcdd85cae04b52a3d2f25595
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-07-01 00:31:15 +0530

    removed promisify and added copyrecursive

--- backend/controllers/revert.js
+++ backend/controllers/revert.js
@@ -1,24 +1,16 @@
 const fs = require("fs");
 const path = require("path");
-const { promisify } = require("util");
-
-const readdir = promisify(fs.readdir);
-const copyFile = promisify(fs.copyFile);
+const { copyRecursive } = require("../utils/fileHelpers");
 
 async function revertRepo(commitID) {
   const repoPath = path.resolve(process.cwd(), ".Commitly");
-  //  const stagingPath = path.join(repoPath, "staging");
   const commitsPath = path.join(repoPath, "commits");
 
   try {
     const commitDir = path.join(commitsPath, commitID);
-    const files = await readdir(commitDir);
     const parentDir = path.resolve(repoPath, "..");
 
-    for (const file of files) {
-      await copyFile(path.join(commitDir, file), path.join(parentDir, file));
-    }
-    await copyRecursive(parentDir, commitDir);
+    await copyRecursive( commitDir, parentDir);
 
     console.log(`Commit ${commitID} reverted successfully!`);
   } catch (err) {
